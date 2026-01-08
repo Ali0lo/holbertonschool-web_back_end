1b0ebac609c8743a6b58a033c08c0731e3c473c2
Ali0lo <aliaze975@gmail.com>
2026-01-08 20:10:43 +0000

    A

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -46,3 +46,24 @@ if __name__ == "__main__":
 
     # Status check
     })
+"""Script that provides stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """Provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection. count_documents({"method": method})
+        print(f"\tmethod {method}:  {count}")
+    # Status check
+    status_check = nginx_collection.count_documents({
+        "method": "GET",
+        "path":  "/status"
+    })
+    print(f"{status_check} status check")
